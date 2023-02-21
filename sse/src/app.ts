2b5d4d0b4044fc90ba1880b758d37d722bad9e54
andcan <kk4r.1m@gmail.com>
2023-02-21 11:58:33 +0000

    add error reproduction

---
+++ sse/src/app.ts
@@ -0,0 +1,79 @@
+import { Socket } from 'net'
+import { createServer } from 'http'
+import { createYoga, createSchema } from 'graphql-yoga'
+import { useGraphQLSSE } from '@graphql-yoga/plugin-graphql-sse'
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export function buildApp() {
+  const yoga = createYoga({
+    graphiql: {
+      // Use graphql-sse in GraphiQL.
+      subscriptionsProtocol: 'GRAPHQL_SSE'
+    },
+    schema: createSchema({
+      typeDefs: /* GraphQL */ `
+        type Query {
+          hello: String!
+        }
+        type Mutation {
+          dontChange: String!
+        }
+        type Subscription {
+          greetings: String!
+        }
+      `,
+      resolvers: {
+        Query: {
+          hello() {
+            return 'world'
+          },
+        },
+        Mutation: {
+          dontChange() {
+            return 'didntChange'
+          },
+        },
+        Subscription: {
+          greetings: {
+            async *subscribe() {
+              for (const hi of ['Hi', 'Bonjour', 'Hola', 'Ciao', 'Zdravo']) {
+                await sleep(30000)
+                yield { greetings: hi }
+              }
+            },
+          },
+        },
+      },
+    }),
+    plugins: [useGraphQLSSE()],
+  })
+
+  const server = createServer(yoga)
+
+  // for termination
+  const sockets = new Set<Socket>()
+  server.on('connection', (socket) => {
+    sockets.add(socket)
+    server.once('close', () => sockets.delete(socket))
+  })
+
+  return {
+    start: (port: number) =>
+      new Promise<void>((resolve, reject) => {
+        server.on('error', (err) => reject(err))
+        server.on('listening', () => resolve())
+        server.listen(port)
+      }),
+    stop: () =>
+      new Promise<void>((resolve) => {
+        for (const socket of sockets) {
+          socket.destroy()
+          sockets.delete(socket)
+        }
+        server.close(() => resolve())
+      }),
+  }
+}
